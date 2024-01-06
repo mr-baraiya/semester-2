c4ee520e4e739f1827c3c54d20f1b40fe38deec5
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2024-01-06 12:32:59 +0530

    Add files via upload

---
+++ Comandlineargument.java
@@ -0,0 +1,8 @@
+public class Comandlineargument {
+        public static void main(String[ ] args){
+            int n1=Integer.parseInt(args[0]);
+            double n2=Double.parseDouble(args[1]);
+            System.out.println(n1);
+            System.out.println(n2);
+        }    
+}
---
+++ Scannerclass.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Scannerclass {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a Ineger Number =");
+        int a=sc.nextInt();
+        System.out.println("Enter a Float  Number =");
+        Float b=sc.nextFloat();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
