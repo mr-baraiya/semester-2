e89b707a6f6baed39b1dfccaae783dbb3899934b
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2024-01-09 10:55:10 +0530

    Add files via upload

---
+++ Lab23.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Lab23 {
+    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the first number =");
+    double a=sc.nextDouble();
+    System.out.print("Enter the second number =");
+    double b=sc.nextDouble();
+    System.out.print("Enter the operation =");
+    //sc.next();for upto space string
+    //sc.nextLine();for full string
+    //char a = sc.next().charAt(0);
+    char str = sc.next().charAt(0);
+    switch (str) {
+        case '+': System.out.println(a+b); break;
+        case '-': System.out.println(a-b); break;
+        case '*': System.out.println(a*b); break;
+        case '/': System.out.println(a/b); break;
+        case '%': System.out.println(a%b); break;
+        default : System.out.println("Wrong Input. \n Please Enter Right Input.");
+    }
+    }
+}
