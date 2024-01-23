7033a1048a5f8bcca18c713aa5616f31ba320f30
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2024-01-23 11:22:37 +0530

    Update Lab46.java

--- Lab46.java
+++ Lab46.java
@@ -11,7 +11,7 @@ public class Lab46 {
             else
             {
                 System.out.println("Error occured");
-                break;
+                return ;
             }
         }
         System.out.println("All is well");
