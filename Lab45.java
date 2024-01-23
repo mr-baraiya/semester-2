5cda8c4842172ef55ccfc8b25ac17d4b0d640ce9
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2024-01-23 10:58:13 +0530

    Add files via upload

---
+++ Lab45.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Lab45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string :-");
+        String str = sc.nextLine();
+        int i = 0;
+        for (char c : str.toCharArray()) {
+            i++;
+        }
+        System.out.println("String Length =" + i);
+        for(int j=i/2 ; j < i ; j++){
+            System.out.print(str.charAt(j));
+        }
+    }
+}
