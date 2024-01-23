7cbc4b2c52dea409c1c74b28ae7aab3554a29315
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2024-01-23 11:11:53 +0530

    Add files via upload

---
+++ Lab46.java
@@ -0,0 +1,19 @@
+/*Write an application that searches through its command-line argument. If an argument is found that does not begin with 
+and upper case letter, display error message and terminate. */
+
+public class Lab46 {
+    public static void main(String[] args){
+        for(int i=0;i<args.length;i++){
+            if(args[i].charAt(0)>=65 &&  args[i].charAt(0)<=91)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println("Error occured");
+                break;
+            }
+        }
+        System.out.println("All is well");
+    }
+}
