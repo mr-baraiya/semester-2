b24903893b79b0900df7521f025b04c371a37153
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2024-02-06 11:02:00 +0530

    Add files via upload

--- Lab_5_3.java
+++ Lab_5_3.java
@@ -1,70 +1,62 @@
-/*Create a class which ask the user to enter a sentence, and it should display count of
-each vowel type in the sentence.
-The program should continue till user enters a word “quit”. Display the total count of each 
-vowel for all sentences.*/
-import java.util.Scanner;
-public class Lab_5_3
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Sentence sentence = new Sentence();
-		do
-		{
-			System.out.println("Enter a sentence(type quit to exit)");
-			String s = sc.nextLine();
-			if(s.equals("quit"))
-			{
-				break;
-			}
-			sentence.processSentence(s);
-		}while(true);
-		System.out.println("a = "+sentence.count_a+"\ne = "+sentence.count_e+"\ni = "+sentence.count_i+"\no = "+sentence.count_o+"\nu = "+sentence.count_u);
-	}
-}
-class Sentence{
-	String s;
-	int count_a=0;
-	int count_e=0;
-	int count_i=0;
-	int count_o=0;
-	int count_u=0;
-	Sentence()
-	{
+import java.util.*;
 
-		this.s = s;
-		this.count_a=count_a;
-		this.count_e=count_e;
-		this.count_i=count_i;
-		this.count_o=count_o;
-		this.count_u=count_u;
-	}
-		public void processSentence(String s)
-		{
+class sentences {
+    String s;
+    int count_a ;
+    int count_e ;
+    int count_i ;
+    int count_u ;
+    int count_o ;
 
-		for(int i=0;i<s.length();i++)
-		{
+    public sentences(){
+		this.count_a = 0;
+		this.count_e = 0;
+		this.count_i = 0;
+		this.count_o = 0;
+		this.count_u = 0;
+    }
 
-			if(s.charAt(i)=='a'||s.charAt(i)=='A')
-			{
+    
+	public void processSentence(String s){
+    for(int i=0;i<s.length();i++){
+
+			if(s.charAt(i)=='a'||s.charAt(i)=='A'){
 				count_a++;
 			}
-			if(s.charAt(i)=='e'||s.charAt(i)=='E')
-			{
+			if(s.charAt(i)=='e'||s.charAt(i)=='E'){
 				count_e++;
 			}
-			if(s.charAt(i)=='i'||s.charAt(i)=='I')
-			{
+			if(s.charAt(i)=='i'||s.charAt(i)=='I'){
 				count_i++;
 			}
-			if(s.charAt(i)=='o'||s.charAt(i)=='O')
-			{
+			if(s.charAt(i)=='o'||s.charAt(i)=='O'){
 				count_o++;
 			}
-			if(s.charAt(i)=='u'||s.charAt(i)=='U')
-			{
+			if(s.charAt(i)=='u'||s.charAt(i)=='U'){
 				count_u++;
 			}
 		}
-		}
+    }
+}
 
-	}
+public class Lab_5_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        sentences sentence = new sentences();
+        while(true){
+            System.out.println("Please Enter String an ( Enter the quit for stop ) = ");
+            String s = sc.nextLine();
+            if(s.equalsIgnoreCase("quit")){
+                break;
+            }
+            else{
+                sentence.processSentence(s);
+            }
+        }
+        System.out.println("a = "+sentence.count_a);
+        System.out.println("e = "+sentence.count_e);
+        System.out.println("i = "+sentence.count_i);
+        System.out.println("o = "+sentence.count_o);
+        System.out.println("u = "+sentence.count_u);
+    }
+}
