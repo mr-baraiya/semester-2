530c55516d9bfb57c9bdc807ecc59f75f1bf09f2
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2024-02-12 19:18:55 +0530

    Add files via upload

---
+++ Lab_5_6.java
@@ -0,0 +1,26 @@
+// WAP that counts the number of objects created using static.
+
+import java.util.Scanner;
+
+class Counter{
+	static int count;
+	Counter(){
+		count++;
+	}
+}
+
+public class Lab_5_6{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter how many objects to be created : ");
+		int n = sc.nextInt();
+		Counter c[] = new Counter[n];
+		for(int i=0;i<n;i++)
+		{
+			 c[i] = new Counter();
+		}
+
+		System.out.println("Total objects created = "+c[n-1].count);
+		sc.close();
+	}
+}
